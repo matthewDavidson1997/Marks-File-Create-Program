8781a1e549b8deb157a67daf5ab841713d193cf4
matthewdavidson1997 <matthewdavidson1997@gmail.com>
2022-11-01 12:58:14 +0000

    Some refactoring (possibly bad)

--- Marks_Files_CSV_Create.py
+++ Marks_Files_CSV_Create.py
@@ -22,6 +22,33 @@ REGEX_PATTERNS = {"Centre": [re.compile(r"^[\d]{5}$"), re.compile(r"^[A-Z]{2}[\d
                   "Mark Scheme": [re.compile(r"^[1-4]$")],
                   "Candidates": [re.compile(r"^(?P<min_cand>[1-9][\d]*)[\s]+(?P<max_cand>[1-9][\d]*)$")]}
 
+
+def get_input_request(key):
+    INPUT_REQUESTS = {"POS": "Enter POS (in format D###): ",
+                      "KAD": "Enter KAD (in format DD/MM/YYYY): ",
+                      "Sitting": "Enter sitting (AM, PM, EV): ",
+                      "Centre": "Enter centre number: ",
+                      "Candidates": "Enter the first and last candidate number (for example '1 10' for 10 candidates): ",
+                      "QPV": "Enter QPV for ",
+                      "Mark Scheme": "Choose an option from the following list:\n\
+                    1: Use full marks for all candidates\n\
+                    2: Use random marks for all candidates\n\
+                    3: Use specific marks for each module but shared by candidates\n\
+                    4: Choose individual marks for each candidate and module\n"}
+    return input(INPUT_REQUESTS[key]).upper()
+
+
+def get_error_message(key):
+    ERROR_MESSAGES = {"POS": "Invalid POS, please try again",
+                      "KAD": "Invalid date given, try again",
+                      "Sitting": "Invalid sitting, please try again",
+                      "Centre": "Invalid centre, please try again",
+                      "Candidates": "Invalid candidate range, please try again",
+                      "QPV": "QPV can only be numerical, please try again",
+                      "Mark Scheme": "Invalid option entered, please try again"}
+    return print(ERROR_MESSAGES[key])
+
+
 def get_pos() -> str:
     """Function to take and validate POS input
 
@@ -29,9 +56,9 @@ def get_pos() -> str:
         str: Validated POS
     """
     while True:
-        pos = input("Enter POS (in format D###): ").upper()
+        pos = get_input_request('POS')
         if pos not in POS_CODES['Programme of Study Code'].unique():
-            print("Invalid POS, please try again")
+            get_error_message('POS')
             continue
         return pos
 
@@ -43,11 +70,11 @@ def get_kad() -> str:
         str: Validated KAD
     """
     while True:
-        user_input = input("Enter KAD (in format DD/MM/YYYY): ").upper()
+        kad = get_input_request('KAD')
         try:
-            kad = datetime.strptime(user_input, '%d/%m/%Y')
+            kad = datetime.strptime(kad, '%d/%m/%Y')
         except ValueError:
-            print("Invalid date given, try again")
+            print(get_error_message('KAD'))
             continue
         return datetime.strftime(kad, '%d/%m/%Y')
 
@@ -59,9 +86,9 @@ def get_sitting() -> str:
         str: Validated sitting
     """
     while True:
-        sitting = input("Enter sitting (AM, PM, EV): ").upper()
+        sitting = get_input_request('Sitting')
         if sitting not in ["AM", "PM", "EV"]:
-            print("Invalid sitting, please try again")
+            get_error_message('Sitting')
             continue
         return sitting
 
@@ -72,33 +99,31 @@ def get_centre() -> str:
     Returns:
         str: validated centre
     """
-    match_question = "Enter centre number: "
-    error_message = "Invalid centre, please try again"
-    return validate_match(regex_pattern=REGEX_PATTERNS['Centre'], match_question=match_question, error_message=error_message)
+    return validate_match(regex_pattern=REGEX_PATTERNS['Centre'], key='Centre')
 
 
 def get_candidates():
     patt = re.compile(r"^(?P<min_cand>[1-9][\d]*)[\s]+(?P<max_cand>[1-9][\d]*)$")
     while True:
-        candidates = input("Enter the first and last candidate number (for example '1 10' for 10 candidates): ")
+        candidates = get_input_request("Candidates")
         candidates = candidates.strip()
         match = patt.match(candidates)
         if not match:
-            print("Invalid candidate range, please try again")
+            get_error_message("Candidates")
             continue
         min_cand = int(match.groupdict()["min_cand"])
         max_cand = int(match.groupdict()["max_cand"])
         if max_cand < min_cand:
-            print("Invalid candidate range, please try again")
+            get_error_message("Candidates")
             continue
         return range(min_cand, max_cand + 1)
 
 
-def validate_match(regex_pattern: List[re.Pattern], match_question: str, error_message: str):
+def validate_match(regex_pattern: List[re.Pattern], key):
     while True:
-        user_input = input(match_question).upper()
+        user_input = get_input_request(key)
         if not any([patt.match(user_input) for patt in regex_pattern]):
-            print(error_message)
+            get_error_message(key)
             continue
         return user_input
 
@@ -106,9 +131,8 @@ def validate_match(regex_pattern: List[re.Pattern], match_question: str, error_m
 def get_qpvs(candidates: range, df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
     for module in df['Module Code'].unique():
-        match_question = f"Enter QPV for {module}: "
-        error_message = "QPVs can only contain numeric digits, please try again"
-        qpv = validate_match(regex_pattern=REGEX_PATTERNS['QPV'], match_question=match_question, error_message=error_message)
+        print(f"{module} : ")
+        qpv = validate_match(regex_pattern=REGEX_PATTERNS['QPV'], key='QPV')
         for cand in candidates:
             # Put entered mark into each row
             df.loc[(df['Module Code'] == module) & (df['Candidate No'] == cand), 'Module Question Paper Version'] = qpv
@@ -122,13 +146,7 @@ def add_details_to_df(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def mark_scheme() -> int:
-    match_question = "Choose an option from the following list:\n\
-        1: Use full marks for all candidates\n\
-        2: Use random marks for all candidates\n\
-        3: Use specific marks for each module but shared by candidates\n\
-        4: Choose individual marks for each candidate and module\n"
-    error_message = "Invalid option entered, please try again"
-    return int(validate_match(regex_pattern=REGEX_PATTERNS['Mark Scheme'], match_question=match_question, error_message=error_message))
+    return int(validate_match(regex_pattern=REGEX_PATTERNS['Mark Scheme'], key='Mark Scheme'))
 
 
 def validate_mark(max_mark: int, input_question: str):
@@ -200,7 +218,7 @@ def add_candidates_to_df(candidates: range, df: pd.DataFrame, candidate_df: pd.D
     return candidate_df
 
 
-def get_qpvs_for_candidates(pos_df, candidate_df):
+def get_qpvs_for_candidates(pos_df: pd.DataFrame, candidate_df: pd.DataFrame) -> pd.DataFrame:
     # create an empty dataframe to store data for all candidates
     choice = "y"
     while choice == "y":
@@ -209,32 +227,19 @@ def get_qpvs_for_candidates(pos_df, candidate_df):
         # Take QPV from user and add to dataframe
         candidate_df = get_qpvs(candidates, candidate_df)
         choice = input("Would you like to add another candidate range? y/n:")
-    # Ask how the user would like to add marks
-    marking_choice = mark_scheme()
-    # Add marks to each candidate
-    candidate_df = assign_marks(candidate_df, marking_choice)
     return candidate_df
 
 
 def save_df_to_csv(df: pd.DataFrame):
     # Drop Max Marks column for output csv
     df = df.drop(columns=['Max_Mark'])
-    kad, pos, centre, sitting = df['Assessment Event Date'].unique(), df['Programme of Study Code'].unique(),\
-        df['Centre No'].unique(), df['Assessment Event Sitting'].unique()
-    
-    kad, pos, centre, sitting = str(kad[0]), pos[0], centre[0], sitting[0]
-
+    kad, pos, centre, sitting = df['Assessment Event Date'].unique()[0], df['Programme of Study Code'].unique()[0],\
+        df['Centre No'].unique()[0], df['Assessment Event Sitting'].unique()[0]
     df['Candidate No'] = df['Candidate No'].apply(lambda x: f"{x:>04}")
     # Remove invalid filepath symbols
     file_kad = kad.replace("/", "")
     # Save df to CSV with filename using session details
     df.to_csv(str(FILEPATH) + f'\marksfile_{pos}_{centre}_{file_kad}{sitting}.csv', index=False)
-
-
-def input_choice():
-    return input("How would you like to enter data? \n\
-        1. Enter in format (POS) (KAD) (Sitting) (Centre)\n\
-        2. Enter separately\n")
 
 
 def main():
@@ -246,8 +251,15 @@ def main():
         print("Marks file creation program")
         # Slice df and add the provided details
         pos_df = POS_CODES[POS_CODES['Programme of Study Code'] == get_pos()].copy()
+        print("I'm here 1")
         pos_df = add_details_to_df(pos_df)
+        print("I'm here 2")
         candidate_df = get_qpvs_for_candidates(pos_df=pos_df, candidate_df=candidate_df)
+        print("I'm here 3")
+        # Ask how the user would like to add marks
+        marking_choice = mark_scheme()
+        # Add marks to each candidate
+        candidate_df = assign_marks(candidate_df, marking_choice)
         save_df_to_csv(candidate_df)
         choice = input("Generate another marks file? y/n:")
 
