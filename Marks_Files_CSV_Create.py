354263f511cd1fab18492bfe655b81251c265d36
Matt Davidson <MED139@student.aru.ac.uk>
2022-11-05 17:47:37 +0000

    Add comments

--- Marks_Files_CSV_Create.py
+++ Marks_Files_CSV_Create.py
@@ -92,9 +92,12 @@ def get_pos() -> str:
     Returns:
         str: Validated POS
     """
+    # Get prompt message and error message from dict using POS as key
+    prompt, repeat = MESSAGE_DICT["POS"]
     while True:
-        prompt, repeat = MESSAGE_DICT["POS"]
+        # Get user input
         pos = input(prompt).upper()
+        # Check entered POS is in list of POS codes
         if pos not in POS_CODES["Programme of Study Code"].unique():
             print(repeat)
             continue
@@ -107,14 +110,16 @@ def get_kad() -> str:
     Returns:
         str: Validated KAD
     """
+    # Get prompt message and error message from dict using KAD as key
+    prompt, repeat = MESSAGE_DICT["KAD"]
     while True:
-        prompt, repeat = MESSAGE_DICT["KAD"]
-        user_input = input(prompt).upper()
+        # Get user input
+        user_input = input(prompt)
+        # Check user input is a valid date format and return as DD/MM/YYYY
         try:
             kad = datetime.strptime(user_input, "%d/%m/%Y")
         except ValueError:
             print(repeat)
-
             continue
         return datetime.strftime(kad, "%d/%m/%Y")
 
@@ -125,9 +130,12 @@ def get_sitting() -> str:
     Returns:
         str: Validated sitting
     """
+    # Get prompt message and error message from dict using KAD as key
+    prompt, repeat = MESSAGE_DICT["Sitting"]
     while True:
-        prompt, repeat = MESSAGE_DICT["Sitting"]
+        # Get user input
         sitting = input(prompt).upper()
+        # Check input against list of acceptable inputs
         if sitting not in ["AM", "PM", "EV"]:
             print(repeat)
             continue
@@ -140,32 +148,41 @@ def get_centre() -> str:
     Returns:
         str: validated centre
     """
+    # Return validated input using Centre as key
     return validate_match(key='Centre')
 
 
 def get_candidates() -> range:
+    # Get valid pattern and messages from dicts
     patt = REGEX_PATTERNS["Candidates"]
+    prompt, repeat = MESSAGE_DICT["Candidates"]
     while True:
-        prompt, repeat = MESSAGE_DICT["Candidates"]
+        # Get user input
         candidates = input(prompt).upper()
         candidates = candidates.strip()
         match = patt.match(candidates)
         if not match:
             print(repeat)
             continue
+        # get min cand and max cand from pattern matches
         min_cand = int(match.groupdict()["min_cand"])
         max_cand = int(match.groupdict()["max_cand"])
+        # Validate that min cand is smaller than max cand
         if max_cand < min_cand:
             print(repeat)
             continue
+        # Return as a range to reduce number of variables being passed
         return range(min_cand, max_cand + 1)
 
 
 def validate_match(key) -> str:
+    # Get valid pattern and messages from dicts
     regex_pattern = REGEX_PATTERNS[key]
     prompt, repeat = MESSAGE_DICT[key]
     while True:
+        # Get user input
         user_input = input(prompt).upper()
+        # Validate user input against pattern
         if not regex_pattern.match(user_input):
             print(repeat)
             continue
@@ -174,120 +191,163 @@ def validate_match(key) -> str:
 
 def get_qpvs(candidates: range, df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
-    REGEX_PATTERNS["QPV"]
+    # Get each unique module code from df
     for module in df['Module Code'].unique():
+        # Get QPV for each unique module
         qpv = validate_match(key='QPV')
         for cand in candidates:
-            # Put entered mark into each row
+            # Enter mark for module for candidate range
             df.loc[
                 (df["Module Code"] == module) & (df["Candidate No"] == cand),
-                "Module Question Paper Version",
-            ] = qpv
+                "Module Question Paper Version"] = qpv
     return df
 
 
 def add_details_to_df(df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
+    # Populate df with details entered by user
     df['Assessment Event Date'], df['Assessment Event Sitting'], df['Centre No'] = get_kad(), get_sitting(), get_centre()
     return df
 
 
 def mark_scheme() -> int:
+    # Get user to choose how to add marks
     return int(validate_match(key='Mark Scheme'))
 
 
 def validate_mark(max_mark: int, input_question: str):
+    # Dict to reference when deciding which column to add mark to
     mark_columns = {'Present': 'Candidate Mark',
                     'Absent': 'Candidate Status'}
     while True:
+        # Get mark from user
         mark = input(input_question).upper()
+        # User input if candidate is absent
         if mark == "A":
+            # Add absent to column returned from dict
             return mark, mark_columns['Absent']
         elif mark.isnumeric():
+            # Validate mark is in valid range (0 - Max Mark inclusive)
             if int(mark) <= max_mark and int(mark) >= 0:
+                # Enter mark into column returned from dict
                 return mark, mark_columns['Present']
             else:
+                # Error if validation fails
                 print("Mark given is outside valid range, please try again")
         else:
+            # Check to see if mark is entered as float,
+            # if it is perform same validations
             try:
                 float(mark)
                 if float(mark) <= max_mark and float(mark) >= 0:
+                    # If mark is floatthen assign it to relevant column
                     return mark, mark_columns['Present']
             except ValueError:
                 print("Mark given is outside valid range, please try again")
 
 
 def assign_marks(df: pd.DataFrame, option: int) -> pd.DataFrame:
-    """_summary_
-
+    """Based on user input given as argument,
+    a different method of assigning marks will execute
     Args:
-        df (pd.DataFrame): _description_
-        option (int): _description_
+        df (pd.DataFrame): Dataframe with candidate information
+        option (int): Choice of mark entry method
 
     Returns:
-        pd.DataFrame: _description_
+        pd.DataFrame: Dataframe with marks
     """
     df = df.copy()
     if option == 1:
         # Set candidate mark as the maximum mark
         df["Candidate Mark"] = df["Max_Mark"]
-
     elif option == 2:
         # Enter a random mark for each row within range of max mark
         df["Candidate Mark"] = df["Max_Mark"].apply(lambda x: randint(0, x))
-
     elif option == 3:
-        # Get each distinct module code
-        print(df["Module Code"].unique())
-        # Loop distinct module codes to assign a mark to all rows matching that module
-        for module in df['Module Code'].unique():
-            # Get the maximum possible mark for that module
-            max_mark = df[df['Module Code'] == module]['Max_Mark'].to_list()[0]
-            input_question = f"Enter mark for {module} (max: {max_mark}): "
-            mark, mark_column = validate_mark(max_mark, input_question)
-            # Put entered mark into each row
-            df.loc[df['Module Code'] == module, mark_column] = mark
-
+        df = enter_marks_for_each_module(df)
     elif option == 4:
-        for idx, row in df.iterrows():
-            # Get the maximum possible mark for that row
-            max_mark = row['Max_Mark']
-            input_question = (
-                f"Enter mark for {row['Module Code']}\
-                {row['Measure Def Code']}\
-                {row['Candidate No']}\
-                (max: {max_mark}): ")
-            mark, mark_column = validate_mark(max_mark, input_question)
-            df.loc[idx, mark_column] = mark
-
+        df = enter_marks_for_every_row(df)
     return df
 
 
-def add_candidates_to_df(
-    candidates: range, df: pd.DataFrame, candidate_df: pd.DataFrame
-        ) -> pd.DataFrame:
+def enter_marks_for_each_module(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+    # Get each distinct module code
+    df["Module Code"].unique()
+
+    # Loop distinct module codes to assign a mark 
+    # to all rows matching that module
+    for module in df['Module Code'].unique():
+        # Get the maximum possible mark for that module
+        max_mark = df[[df['Module Code'] == module]['Max_Mark']].to_list()[0]
+
+        # Ask user for mark for module
+        input_question = f"Enter mark for {module} (max: {max_mark}): "
+
+        # Validate user entered mark
+        mark, mark_column = validate_mark(max_mark, input_question)
+
+        # Put entered mark into each row
+        df.loc[df['Module Code'] == module, mark_column] = mark
+    return df
+
+
+def enter_marks_for_every_row(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+    for idx, row in df.iterrows():
+        # Get the maximum possible mark for given row
+        max_mark = row['Max_Mark']
+
+        # Ask user for for input with useful prompts
+        input_question = (
+            f"Enter mark for {row['Module Code']}\
+            {row['Measure Def Code']}\
+            {row['Candidate No']}\
+            (max: {max_mark}): ")
+
+        # Validate entered mark
+        mark, mark_column = validate_mark(max_mark, input_question)
+
+        # Add validated mark to corresponding df location
+        df.loc[idx, mark_column] = mark
+    return df
+
+
+def add_candidates_to_df(candidates: range, df: pd.DataFrame,
+                         candidate_df: pd.DataFrame) -> pd.DataFrame:
     # For each candidate in the range of candidates given add candidate number
     # to a new column in dataframe
     for cand in candidates:
+        # Create a temp df to hold data for each candidate
         temp_df = df.copy()
+
+        # Set candidate number
         temp_df["Candidate No"] = cand
+
         # Concatenate the dataframe for each candidate
         candidate_df = pd.concat([candidate_df, temp_df], ignore_index=True)
+
+    # Change max mark type float -> int
     candidate_df["Max_Mark"] = candidate_df["Max_Mark"].astype(np.uint8)
     return candidate_df
 
 
 def get_qpvs_for_candidates(pos_df: pd.DataFrame, candidate_df: pd.DataFrame) -> pd.DataFrame:
-    # create an empty dataframe to store data for all candidates
-    choice = "y"
-    while choice == "y":
+    # Repeat as long as user wants to add more candidates
+    choice = "Y"
+    while choice == "Y":
+        # Get candidate numbers
         candidates = get_candidates()
         candidate_df = add_candidates_to_df(
-            candidates=candidates, df=pos_df, candidate_df=candidate_df
-        )
+            candidates=candidates, 
+            df=pos_df, 
+            candidate_df=candidate_df)
+
         # Take QPV from user and add to dataframe
         candidate_df = get_qpvs(candidates, candidate_df)
-        choice = input("Would you like to add another candidate range? y/n:")
+        
+        # Ask user if they want to add more candidates
+        choice = input("Would you like to add another candidate range? y/n: ").upper()
     return candidate_df
 
 
